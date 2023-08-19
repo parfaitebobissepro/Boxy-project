ca253e24fb2d101070269179c9500c8950fbf42e
parfaitebobissepro <parfaitebobissepro@gmail.com>
2023-08-20 00:19:39 +0200

    mise en place #done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./assets/components/Header";
+import Footer from "./assets/components/Footer";
+
 function App() {
   return (
-    <div>
-
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 mb:flex-wrap">
+        {/* <LeftSide /> */}
+        {/* <Visualisation /> */}
+      </main>
+      <Footer />
     </div>
   );
 }
