ecbedae3635b8e800a692047fa382f2b39fe7af7
parfaitebobissepro <parfaitebobissepro@gmail.com>
2023-08-21 11:05:11 +0200

    left container #progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Header from "./assets/components/Header";
-import Footer from "./assets/components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Leftcontainer from "./layouts/Leftcontainer";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 mb:flex-wrap">
-        {/* <LeftSide /> */}
+        <Leftcontainer />
         {/* <Visualisation /> */}
       </main>
       <Footer />
